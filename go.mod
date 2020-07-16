35162dec7d11d33ff3dd74e12b15283b02947391
akouryy <akouryy7@gmail.com>
2020-07-16 21:21:31 +0900

    Add database access

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
-module github.com/akouryy/api.ss.pyon.app
+module github.com/akouryy/ss-api.pyon.app
 
 go 1.14
 
-require github.com/zenazn/goji v1.0.1
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/zenazn/goji v1.0.1
+)
--- src/main.go
+++ src/main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 )
 
+var db *sqlx.DB
+
+type Book struct {
+	Id        string
+	Title     string
+	CreatedAt time.Time
+}
+
 func booksHandler(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "{ message: \"Hello, world!\" }")
+	w.Header().Set("Content-Type", "application/json")
+
+	books := []Book{}
+	db.Select(&books, "SELECT * FROM books ORDER BY created_at DESC LIMIT 10")
+
+	j, err := json.Marshal(books)
+	if err != nil {
+		fmt.Fprintf(w, "error %s", err.Error())
+		return
+	}
+	io.WriteString(w, string(j))
 }
 
 func main() {
+	var err error
+
+	db, err = sqlx.Connect("mysql", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	goji.Get("/books", booksHandler)
 	goji.Serve()
 }
